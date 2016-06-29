b9fe6e1420ddec28abf4d03bb7c6331967cd6087
Vittorio Rigamonti <rigazilla@gmail.com>
2016-06-29 18:37:57 +0200

    initial commit

---
+++ GlassRod/HotRodUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassRod
+{
+    class HotRodUtils
+    {
+        public static List<byte> vIntegerToBytes(ulong value)
+        {
+            List<byte> ret = new List<byte>();
+            while (value > 0)
+            {
+                byte b = (byte)(value % 0x7f);
+                value >>= 7;
+                if (value > 0)
+                    b |= 0x80;
+                ret.Add(b);
+
+            }
+            return ret;
+        }
+
+        public static byte[] stringToBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static ulong bytesToVLong(List<byte> bytes, ref ulong pos)
+        {
+            ulong ret = 0;
+            do
+            {
+                ret += (ulong)(bytes[(int)pos] & 0x7f);
+            } while ((bytes[(int)pos++] | 0x80) != 0);
+            return ret;
+        }
+
+        internal static ushort bytesToUShort(List<byte> bytes, ref ulong pos)
+        {
+            return (ushort)((int)bytes[(int)pos++] << 8 + bytes[(int)pos++]);
+        }
+
+        internal static ulong bytesToULong(List<byte> bytes, ref ulong pos)
+        {
+            ulong ret = 0;
+            for (int i=0; i<4; i++)
+            {
+                ret <<= 8;
+                ret += bytes[(int)pos++];
+            }
+            return ret;
+        }
+    }
+}
---
+++ GlassRod/ResponseHeader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+namespace GlassRod
+{
+    public class HashAwareNodeAddress
+    {
+        public string address;
+        public ushort port;
+        public ulong hashCode;
+        public HashAwareNodeAddress(string address, ushort port, ulong hashCode)
+        {
+            this.address = address;
+            this.port = port;
+            this.hashCode = hashCode;
+        }
+    }
+    public class HashAwareTopologyInfo
+    {
+        public ulong topologyId;
+        public ushort numKeyOwner;
+        public byte hashFuncVersion;
+        public ulong hashSpaceSize;
+        public ulong numNodes;
+        public List<HashAwareNodeAddress> nodes;
+
+        private HashAwareTopologyInfo()
+        {
+        }
+
+        public static HashAwareTopologyInfo fromBytes(List<Byte> bytes)
+        {
+            HashAwareTopologyInfo resp = new HashAwareTopologyInfo();
+            ulong pos = 0;
+            resp.topologyId = HotRodUtils.bytesToVLong(bytes, ref pos);
+            resp.numNodes = HotRodUtils.bytesToVLong(bytes, ref pos);
+            resp.nodes = new List<HashAwareNodeAddress>();
+            for (uint i=0; i < resp.numNodes; i++)
+            {
+                ulong addressLength = HotRodUtils.bytesToVLong(bytes, ref pos);
+                string address = Encoding.UTF8.GetString(bytes.GetRange((int)pos, (int)addressLength).ToArray());
+                pos += addressLength;
+                ushort port = HotRodUtils.bytesToUShort(bytes, ref pos);
+                ulong hashcode = HotRodUtils.bytesToULong(bytes, ref pos);
+                resp.nodes.Add(new HashAwareNodeAddress(address, port, hashcode));
+            }
+            return resp;
+        }
+    }
+
+    public class ResponseHeader
+    {
+        public byte magic;
+        public ulong messageId;
+        public byte opcode;
+        public byte status;
+        public byte topologyChangeMarker;
+        public HashAwareTopologyInfo topologyInfo;
+        private ResponseHeader()
+        {
+        }
+        static ResponseHeader fromBytes(List<Byte> bytes)
+        {
+            ResponseHeader resp = new ResponseHeader();
+            ulong pos = 0;
+            if (bytes[0] != 0xA1)
+            {
+                throw new Exception("Bad magic number received: " + bytes[0]);
+            }
+            resp.magic = bytes[0];
+
+            resp.messageId = HotRodUtils.bytesToVLong(bytes, ref pos);
+            resp.opcode = bytes[(int)pos++];
+            resp.status = bytes[(int)pos++];
+            resp.topologyChangeMarker = bytes[(int)pos++];
+
+            if (resp.topologyChangeMarker != 0)
+            {
+                List<Byte> topologyList = bytes.GetRange((int)pos, bytes.Count);
+                HashAwareTopologyInfo.fromBytes(topologyList);
+            }
+            return resp;
+        }
+    }
+}
